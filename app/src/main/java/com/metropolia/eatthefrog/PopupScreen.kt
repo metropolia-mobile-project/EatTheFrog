6ff05250f9b51495ba14898ad3291026f2e22c57
AOskari <oskari.arponen@hotmail.fi>
2022-10-31 16:35:01 +0200

    further popup implementation

--- app/src/main/java/com/metropolia/eatthefrog/PopupScreen.kt
+++ app/src/main/java/com/metropolia/eatthefrog/PopupScreen.kt
@@ -1,40 +1,91 @@
 package com.metropolia.eatthefrog
 
+import android.renderscript.RenderScript
+import android.util.Log
+import androidx.compose.animation.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
-class PopupScreen {
+/**
+ * A Popup window which has a slide in animation. Can be used to display wanted UI components within the window.
+ * @param slideAnimationSpeed: determines how fast the PopupView opens up when opened.
+ * @param openedPopup: The opened state of the PopupView.
+ * @param content: The content displayed within the PopupView.
+ */
+@Composable
+fun PopupView(slideAnimationSpeed : Int = 200, openedPopup: MutableState<Boolean>, content: @Composable () -> Unit) {
 
+    var popupStatus by remember { openedPopup }
 
-    @Composable
-    fun view() {
-
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom) {
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Blue),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text("Popup window")
-            }
-        }
-
-
-
-
+    fun closePopup() {
+        popupStatus = false
     }
 
+    Column(
+        Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        AnimatedVisibility(visible = popupStatus,
+            enter = slideInVertically(
+                initialOffsetY = { it },
+                animationSpec = tween(
+                    durationMillis = slideAnimationSpeed,
+                    easing = LinearEasing
+                )
+            ),
+            exit = slideOutVertically(
+                targetOffsetY = { it },
+                animationSpec = tween(
+                    durationMillis = slideAnimationSpeed - 100,
+                    easing = LinearEasing
+                )
+            )
+        ){
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .alpha(0.6f)
+                    .background(Color.Black),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxHeight(0.2f)
+                        .fillMaxWidth()
+                        .clickable(interactionSource = remember(::MutableInteractionSource), indication = null) { closePopup() },
+                ) {}
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .background(Color.Transparent)
+                        .clip(RoundedCornerShape(topStart = 50.dp, topEnd = 50.dp))
+                        .background(MaterialTheme.colors.secondary),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    content()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/metropolia/eatthefrog/activities/MainActivity.kt
+++ app/src/main/java/com/metropolia/eatthefrog/activities/MainActivity.kt
@@ -3,68 +3,46 @@ package com.metropolia.eatthefrog.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Button
-import androidx.compose.ui.tooling.preview.Preview
+import com.metropolia.eatthefrog.PopupView
 import com.metropolia.eatthefrog.screens.MainScreen
 import com.metropolia.eatthefrog.ui.theme.EatTheFrogTheme
 
 class MainActivity : ComponentActivity() {
 
-
+    var popupVisible = mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             EatTheFrogTheme {
                 MainScreen()
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
-                    testPopup()
+
+                    Button(onClick = {  popupVisible.value = !popupVisible.value }) {
+                        Text("Open test popup")
+                    }
+
+                    Column(Modifier.fillMaxSize()) {
+                        PopupView(200, popupVisible) {
+                            Text("Popup window test")
+                        }
+                    }
+                    
+
                 }
             }
         }
     }
-}
-
-
-@Composable
-fun DefaultPreview() {
-    EatTheFrogTheme {
-        MainScreen()
-fun testPopup() {
-
-    var popupStatus by remember { mutableStateOf(false) }
-
-    val asd = PopupScreen()
-
-    Column(
-        Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(modifier = Modifier.fillMaxWidth(), onClick = {popupStatus = !popupStatus }) {
-            Text("Open popup")
-        }
-
-        AnimatedVisibility(visible = popupStatus) {
-            asd.view()
-        }
-
-    }
-
 }
