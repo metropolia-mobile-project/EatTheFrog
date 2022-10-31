7e23e47157e379cfeb47aa2ba01e7d323fa4aa06
Markus Nivasalo <markus.nivasalo@outlook.com>
2022-10-31 15:42:39 +0200

    Added basic welcome screen that runs when launching the app for the first time

---
+++ app/src/main/java/com/metropolia/eatthefrog/screens/WelcomeScreen.kt
@@ -0,0 +1,150 @@
+package com.metropolia.eatthefrog.screens
+
+import android.app.Activity
+import com.metropolia.eatthefrog.R
+import android.app.Application
+import android.content.Intent
+import android.view.ViewTreeObserver
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.metropolia.eatthefrog.activities.MainActivity
+import com.metropolia.eatthefrog.viewmodels.WelcomeScreenViewModel
+import kotlinx.coroutines.launch
+
+/**
+ * Welcome screen that displays when launching the application for the first time. Contains basic
+ * description of the application as well as an input field to enter your name as the user. Name
+ * is stored to shared preferences. After the name is added and the Save-button is pressed, relaunches
+ * MainActivity which then checks if there is a name stored to the shared preferences and therefore
+ * doesn't display this screen again.
+ */
+@Composable
+fun WelcomeScreen(application: Application, activity: Activity) {
+    var username by remember { mutableStateOf("")}
+    val welcomeScreenViewModel = WelcomeScreenViewModel(application)
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+
+    // Adds listener to the software keyboard to scroll the screen to the bottom when keyboard is opened
+    val view = LocalView.current
+    DisposableEffect(view) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val isKeyboardOpen = ViewCompat.getRootWindowInsets(view)
+                ?.isVisible(WindowInsetsCompat.Type.ime()) ?: true
+            if (isKeyboardOpen) {
+                coroutineScope.launch {
+                    scrollState.animateScrollBy(1000F)
+                }
+            }
+        }
+
+        view.viewTreeObserver.addOnGlobalLayoutListener(listener)
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+
+    Column(
+        Modifier
+            .verticalScroll(scrollState)
+            .fillMaxSize()
+            .background(MaterialTheme.colors.surface)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_frog),
+            contentDescription = "Frog logo",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(20.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp,
+        )
+        Text(
+            text = stringResource(id = R.string.eat_the_frog_description_first),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(20.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+        )
+        Text(
+            text = stringResource(id = R.string.eat_the_frog_description_two),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(20.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+        )
+        Text(
+            text = stringResource(id = R.string.eat_the_frog_description_three),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(20.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+        )
+        Text(
+            text = "Welcome,\nplease enter your name:",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(20.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp,
+        )
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = {
+                welcomeScreenViewModel.savePreferences(username)
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(activity, intent, null)
+            }
+        ) {
+            Text(text = stringResource(R.string.save))
+        }
+    }
+}
