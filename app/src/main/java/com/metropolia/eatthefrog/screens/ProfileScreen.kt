e9d8725b93a923792f8bba9f81d8f5f962f64ba7
Jani Salo <Janikristian.salo@gmail.com>
2022-10-31 23:58:39 +0200

    Basic UI for profile screen, functionality for picking profile picture

--- app/src/main/java/com/metropolia/eatthefrog/screens/ProfileScreen.kt
+++ app/src/main/java/com/metropolia/eatthefrog/screens/ProfileScreen.kt
@@ -1,34 +1,297 @@
 package com.metropolia.eatthefrog.screens
 
+
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.metropolia.eatthefrog.R
+
 
 @Composable
 fun ProfileScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.surface)
-            .wrapContentSize(Alignment.Center)
+            .background(MaterialTheme.colors.secondary)
+            .padding(0.dp, 45.dp, 0.dp, 0.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
+
+        PickImageFromGallery()
+
+
+
+
         Text(
-            text = "Profile View",
+            text = "User Name",
             fontWeight = FontWeight.Bold,
             color = Color.Black,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(15.dp),
             textAlign = TextAlign.Center,
-            fontSize = 25.sp
+            fontSize = 25.sp,
         )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .padding(50.dp, 30.dp, 50.dp, 0.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(130.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Closed tasks",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    textDecoration = TextDecoration.Underline
+                )
+                Text(
+                    text = "1",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Yellow,
+                    fontSize = 18.sp
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .width(130.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                Text(
+                    text = "Frogs eaten",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    textDecoration = TextDecoration.Underline
+                )
+                Text(
+                    text = "1",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Yellow,
+                    fontSize = 18.sp
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .padding(50.dp, 30.dp, 50.dp, 0.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(130.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                Text(
+                    text = "Active tasks",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    textDecoration = TextDecoration.Underline
+                )
+                Text(
+                    text = "1",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Yellow,
+                    fontSize = 18.sp
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .width(130.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                Text(
+                    text = "Total tasks",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    textDecoration = TextDecoration.Underline
+                )
+                Text(
+                    text = "1",
+                    fontWeight = FontWeight.Medium,
+                    color = Color.Yellow,
+                    fontSize = 18.sp
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp, 20.dp, 0.dp, 0.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.End
+            ) {
+                ProfileSwitches()
+                Text(
+                    text = "Dark mode",
+                    fontWeight = FontWeight.Light,
+                    color = Color.Black,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .padding(0.dp, 14.dp, 0.dp, 0.dp)
+
+                )
+            }
+            Row(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.End
+            ) {
+                ProfileSwitches()
+                Text(
+                    text = "Deadline reminder",
+                    fontWeight = FontWeight.Light,
+                    color = Color.Black,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .padding(0.dp, 6.dp, 0.dp, 0.dp)
+                )
+            }
+        }
     }
+}
+
+@Composable
+fun ProfileSwitches() {
+    val checkedState = remember { mutableStateOf(false) }
+    Switch(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it },
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = MaterialTheme.colors.primaryVariant
+        )
+
+    )
+}
+
+
+/**
+ * Creates placeholder image for profile picture and a button that opens gallery
+ * so user can choose their own profile picture.
+ */
+@Composable
+fun PickImageFromGallery() {
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val context = LocalContext.current
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    Box(
+        modifier = Modifier
+            .width(250.dp)
+            .height(170.dp)
+    ) {
+        if (imageUri === null) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile),
+                contentDescription = "Circle Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .border(5.dp, Color.White, CircleShape)
+                    .align(Alignment.Center)
+            )
+        } else {
+
+            imageUri?.let {
+                if (Build.VERSION.SDK_INT < 28) {
+                    bitmap.value = MediaStore.Images
+                        .Media.getBitmap(context.contentResolver, it)
+                } else {
+                    val source = ImageDecoder.createSource(context.contentResolver, it)
+                    bitmap.value = ImageDecoder.decodeBitmap(source)
+                }
+
+                bitmap.value?.let { btm ->
+                    Image(
+                        bitmap = btm.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .clip(CircleShape)
+                            .border(5.dp, Color.White, CircleShape)
+                            .align(Alignment.Center)
+                    )
+                }
+            }
+        }
+        TextButton(modifier = Modifier
+            .width(60.dp)
+            .height(60.dp)
+            .align(Alignment.BottomEnd),
+            onClick = { launcher.launch("image/*") }) {
+            Image(
+                painterResource(id = R.drawable.ic_image),
+                contentDescription = null,
+                Modifier
+                    .width(60.dp)
+                    .height(60.dp),
+            )
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun profileScreenPreview() {
+    ProfileScreen()
 }
