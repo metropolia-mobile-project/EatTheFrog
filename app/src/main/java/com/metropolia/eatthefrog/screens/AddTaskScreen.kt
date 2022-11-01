f993ebba8c620941403b4e1ad429d09a291e7cf7
Jani Salo <Janikristian.salo@gmail.com>
2022-11-01 19:39:07 +0200

    Hide keyboard when clicking screen

--- app/src/main/java/com/metropolia/eatthefrog/screens/AddTaskScreen.kt
+++ app/src/main/java/com/metropolia/eatthefrog/screens/AddTaskScreen.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -22,13 +24,16 @@ import com.metropolia.eatthefrog.R
 import java.text.SimpleDateFormat
 import java.util.*
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun AddTaskScreen() {
+    val keyboardController = LocalSoftwareKeyboardController.current
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
             .background(MaterialTheme.colors.secondary)
+            .clickable { keyboardController?.hide() }
 
     ) {
         UpperPart()
