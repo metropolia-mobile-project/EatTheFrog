c8224c6c3df9dc4d04be76509d9b526e28b86a03
Jani Salo <Janikristian.salo@gmail.com>
2022-11-07 23:48:34 +0200

    Task now needs name and desc to save, subtask max count 7, subtask needs title to be added to list

--- app/src/main/java/com/metropolia/eatthefrog/screens/AddTaskScreen.kt
+++ app/src/main/java/com/metropolia/eatthefrog/screens/AddTaskScreen.kt
@@ -5,11 +5,11 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.util.Log
 import android.widget.DatePicker
+import android.widget.Toast
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -53,7 +53,7 @@ fun AddTaskScreen(application: Application) {
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .background(MaterialTheme.colors.secondary)
+            .background(Color.White)
             .clickable { keyboardController?.hide() }
 
 
@@ -66,7 +66,7 @@ fun AddTaskScreen(application: Application) {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+
 @Composable
 fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
 
@@ -99,7 +99,7 @@ fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
     var taskTitle by remember { mutableStateOf("") }
     var taskType: TaskType by remember { mutableStateOf(taskTypeList[0]) }
     val sDate = remember { mutableStateOf(currentDate) }
-    val sTime = remember { mutableStateOf(currentTime) }
+    val sTime = remember { mutableStateOf("16:00") }
     val newTask =
         Task(0, taskTitle, description, taskType, sDate.value, sTime.value, false, false)
 
@@ -113,8 +113,7 @@ fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
     var subTaskId: Long by remember { mutableStateOf(0) }
     val subTaskDone by remember { mutableStateOf(false) }
 
-    val bringIntoViewRequester = remember { BringIntoViewRequester() }
-    val coroutineScope = rememberCoroutineScope()
+
 
 
 
@@ -134,227 +133,233 @@ fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
     )
 
 
-    /**
-     * Profile picture, task name and text-field for task name
-     */
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(125.dp)
-            .padding(20.dp, 30.dp, 0.dp, 0.dp),
-        verticalAlignment = Alignment.Top,
-        horizontalArrangement = Arrangement.Start
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_profile),
-            contentDescription = "Circle Image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-                .border(5.dp, Color.White, CircleShape)
-        )
-        Column(
-            modifier = Modifier
-                .padding(15.dp, 0.dp, 0.dp, 0.dp),
-        ) {
-            Text(
-                text = stringResource(id = R.string.task_name)
-            )
-            Box(
-                modifier = Modifier
-                    .padding(0.dp),
-                contentAlignment = Alignment.CenterStart,
-            )
-            {
 
-                TextField(
-                    value = taskTitle,
-                    onValueChange = { taskTitle = it },
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.Transparent
-                    ),
-                    singleLine = true,
-                    textStyle = LocalTextStyle.current.copy(
-                        textAlign = TextAlign.Start
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(0.dp, 0.dp, 30.dp, 15.dp),
 
-                    )
-            }
-        }
-    }
-    /**
-     * Description title and TextField
-     * Dropdown menu for picking task type
-     */
-    Column(
-        modifier = Modifier
-            .wrapContentHeight()
-            .wrapContentWidth()
-            .padding(40.dp, 30.dp, 0.dp, 0.dp),
-
-        ) {
-        Row() {
-            Text(
-                text = stringResource(id = R.string.description)
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.TopStart)
-                    .padding(start = 110.dp, end = 30.dp)
-            ) {
-                Text(
-                    items[selectedIndex],
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable(onClick = { expanded = true })
-                        .background(
-                            Color.Transparent
-                        )
-                )
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .background(
-                            MaterialTheme.colors.primary
-                        )
-                ) {
-                    items.forEachIndexed { index, s ->
-                        DropdownMenuItem(onClick = {
-                            selectedIndex = index
-                            expanded = false
-                            taskType = taskTypeList[selectedIndex]
-                        }) {
-                            val disabledText = if (s == disabledValue) {
-                                " (Disabled)"
-                            } else {
-                                ""
-                            }
-                            Text(text = s + disabledText)
-                        }
-                    }
-                }
-                Image(
-                    painter = painterResource(id = R.drawable.ic_dropdownarrow),
-                    contentDescription = "",
-                    modifier = Modifier
-                        .width(25.dp)
-                        .height(25.dp)
-                        .align(Alignment.CenterEnd)
-                )
-            }
-        }
-
-        TextField(
-            value = description,
-            onValueChange = { description = it },
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.Transparent
-            ),
+        /**
+         * Profile picture, task name and text-field for task name
+         */
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(0.dp, 0.dp, 30.dp, 15.dp)
-        )
-    }
-    /**
-     * UI for picking task date and time
-     */
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = CenterVertically,
-        modifier = Modifier
-            .padding(30.dp)
-            .fillMaxWidth()
-    ) {
+                .height(125.dp)
+                .padding(20.dp, 30.dp, 0.dp, 0.dp),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile),
+                contentDescription = "Circle Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(75.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, Color.Black, CircleShape)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(15.dp, 0.dp, 0.dp, 0.dp),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.task_name)
+                )
+                Box(
+                    modifier = Modifier
+                        .padding(0.dp),
+                    contentAlignment = Alignment.CenterStart,
+                )
+                {
+
+                    TextField(
+                        value = taskTitle,
+                        onValueChange = { taskTitle = it },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color.Transparent
+                        ),
+                        singleLine = true,
+                        textStyle = LocalTextStyle.current.copy(
+                            textAlign = TextAlign.Start
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 0.dp, 30.dp, 15.dp),
+
+                        )
+                }
+            }
+        }
+
+        /**
+         * Description title and TextField
+         * Dropdown menu for picking task type
+         */
         Column(
             modifier = Modifier
-                .padding(0.dp, 0.dp, 0.dp, 0.dp)
-        ) {
-            Text(
-                text = stringResource(id = R.string.starting_date)
-            )
-            Column {
-                Row {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_calendar),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .width(25.dp)
-                            .height(25.dp)
-                            .padding(0.dp, 0.dp, 4.dp, 0.dp)
-                    )
+                .wrapContentHeight()
+                .wrapContentWidth()
+                .padding(40.dp, 30.dp, 0.dp, 0.dp),
+
+            ) {
+            Row() {
+                Text(
+                    text = stringResource(id = R.string.description)
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.TopStart)
+                        .padding(start = 110.dp, end = 30.dp)
+                ) {
                     Text(
-                        text = sDate.value,
+                        items[selectedIndex],
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable(onClick = { expanded = true })
+                            .background(
+                                Color.Transparent
+                            )
                     )
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .background(
+                                MaterialTheme.colors.primary
+                            )
+                    ) {
+                        items.forEachIndexed { index, s ->
+                            DropdownMenuItem(onClick = {
+                                selectedIndex = index
+                                expanded = false
+                                taskType = taskTypeList[selectedIndex]
+                            }) {
+                                val disabledText = if (s == disabledValue) {
+                                    " (Disabled)"
+                                } else {
+                                    ""
+                                }
+                                Text(text = s + disabledText)
+                            }
+                        }
+                    }
                     Image(
                         painter = painterResource(id = R.drawable.ic_dropdownarrow),
                         contentDescription = "",
                         modifier = Modifier
-                            .clickable { sDatePickerDialog.show() }
                             .width(25.dp)
                             .height(25.dp)
+                            .align(Alignment.CenterEnd)
                     )
-
                 }
-                Divider(
-                    color = Color.Black, thickness = 2.dp, modifier = Modifier
-                        .width(125.dp)
-                )
             }
-        }
 
-        Column(
+            TextField(
+                value = description,
+                onValueChange = { description = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 0.dp, 30.dp, 15.dp)
+            )
+        }
+        /**
+         * UI for picking task date and time
+         */
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = CenterVertically,
             modifier = Modifier
-                .padding(0.dp, 0.dp, 0.dp, 0.dp)
+                .padding(30.dp)
+                .fillMaxWidth()
         ) {
+            Column(
+                modifier = Modifier
+                    .padding(0.dp, 0.dp, 0.dp, 0.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.starting_date)
+                )
+                Column(Modifier.clickable { sDatePickerDialog.show() }) {
+                    Row {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_calendar),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .width(25.dp)
+                                .height(25.dp)
+                                .padding(0.dp, 0.dp, 4.dp, 0.dp)
+                        )
+                        Text(
+                            text = sDate.value,
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_dropdownarrow),
+                            contentDescription = "",
+                            modifier = Modifier
+                                //.clickable { sDatePickerDialog.show() }
+                                .width(25.dp)
+                                .height(25.dp)
+                        )
+
+                    }
+                    Divider(
+                        color = Color.Black, thickness = 2.dp, modifier = Modifier
+                            .width(125.dp)
+                    )
+                }
+            }
 
             Column(
                 modifier = Modifier
-                    .padding(30.dp, 0.dp, 0.dp, 0.dp)
+                    .padding()
             ) {
                 Text(
-                    text = stringResource(id = R.string.task_deadline)
+                    text = stringResource(id = R.string.task_deadline),
+                    modifier = Modifier.padding(start = 30.dp)
                 )
-                Row(
+                Column(
                     modifier = Modifier
-                        .padding(0.dp, 0.dp, 0.dp, 0.dp)
+                        .padding(30.dp, 0.dp, 0.dp, 0.dp)
+                        .clickable { mTimePickerDialog.show() }
                 ) {
-                    Text(
-                        text = sTime.value,
-                        modifier = Modifier
-                            .padding(0.dp, 0.dp, 65.dp, 0.dp)
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_add_time),
-                        contentDescription = "",
-                        modifier = Modifier
-                            .clickable { mTimePickerDialog.show() }
-                            .width(25.dp)
-                            .height(25.dp)
 
+                    Row(
+                        modifier = Modifier
+                            .padding(0.dp, 0.dp, 0.dp, 0.dp)
+                    ) {
+                        Text(
+                            text = sTime.value,
+                            modifier = Modifier
+                                .padding(0.dp, 0.dp, 65.dp, 0.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_add_time),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .clickable { mTimePickerDialog.show() }
+                                .width(25.dp)
+                                .height(25.dp)
+
+                        )
+                    }
+                    Divider(
+                        color = Color.Black, thickness = 2.dp, modifier = Modifier
+                            .width(125.dp)
                     )
+
                 }
-                Divider(
-                    color = Color.Black, thickness = 2.dp, modifier = Modifier
-                        .width(125.dp)
-                )
-
             }
         }
-    }
+
 
     /**
      * Shows created subtasks in LazyColumn
      */
 
     Column(
-        Modifier.heightIn(0.dp, 200.dp)
+        Modifier.heightIn(0.dp, 350.dp)
     ) {
         Text(
             "Subtasks",
@@ -400,7 +405,7 @@ fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
                 }
             }
         }
-    }
+
 
 
     /**
@@ -438,12 +443,15 @@ fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
                                     1
                                 } else lastTask.value!!.uid + 1
                                 val list = listOf(Subtask(0, subTaskId, subTaskTitle, subTaskDone))
-                                viewModel.updateSubTaskList(list)
-                                subTaskTitle = ""
+                                if(subList.value!!.size < 7 && subTaskTitle != "") { viewModel.updateSubTaskList(list) ; subTaskTitle = "" }
+                                else if (subTaskTitle == "") { Toast.makeText(context, "Subtask must have title", Toast.LENGTH_SHORT).show()}
+                                else { Toast.makeText(context, "Maximum amount of subtasks reached", Toast.LENGTH_SHORT).show()}
+
                             })
                 }
             )
         }
+    }
     }
     /**
      * "Create Task" button
@@ -455,15 +463,23 @@ fun AddTaskScreenC(viewModel: AddTaskScreenViewModel) {
     ) {
         Button(
             onClick = {
-                Log.d("Testing", newTask.toString())
-                Log.d("Testing dropdown", selectedIndex.toString())
-                viewModel.insertTask(newTask)
-                Log.d("Last Task", subTaskId.toString())
-                Log.d("Observable", subList.value.toString())
-                viewModel.insertSubTask()
-                taskTitle = ""
-                description = ""
-                viewModel.clearSubTaskList()
+                if(newTask.name == "" && newTask.description == "") {
+                    Toast.makeText(context, "Task needs name and description", Toast.LENGTH_SHORT).show()
+                }
+                else if (newTask.name == "") {
+                    Toast.makeText(context, "Give task a name", Toast.LENGTH_SHORT).show()
+                }
+                else if (newTask.description == "") {
+                    Toast.makeText(context, "Give task a description", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    viewModel.insertTask(newTask)
+                    viewModel.insertSubTask()
+                    taskTitle = ""
+                    description = ""
+                    viewModel.clearSubTaskList()
+                }
+
 
             }, modifier = Modifier
                 .width(200.dp)
