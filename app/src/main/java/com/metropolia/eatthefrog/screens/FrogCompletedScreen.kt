af19f7cb7c05da7e28dc191236748ea01b7742d3
AOskari <oskari.arponen@hotmail.fi>
2022-11-20 20:56:50 +0200

    merge master + cleanup

--- app/src/main/java/com/metropolia/eatthefrog/screens/FrogCompletedScreen.kt
+++ app/src/main/java/com/metropolia/eatthefrog/screens/FrogCompletedScreen.kt
@@ -3,13 +3,9 @@ package com.metropolia.eatthefrog.screens
 import androidx.compose.animation.*
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
-import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
