5d36752c3fdff3cc750fb29c31bb2c63157fd67b
AOskari <oskari.arponen@hotmail.fi>
2022-11-23 01:29:01 +0200

    placeholder for empty LazyColumn

--- app/src/main/java/com/metropolia/eatthefrog/screens/history/components/CompletedTasksContainer.kt
+++ app/src/main/java/com/metropolia/eatthefrog/screens/history/components/CompletedTasksContainer.kt
@@ -1,16 +1,22 @@
 package com.metropolia.eatthefrog.screens.history.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.metropolia.eatthefrog.R
 import com.metropolia.eatthefrog.constants.DATE_FORMAT
 import com.metropolia.eatthefrog.viewmodels.HistoryScreenViewModel
@@ -21,24 +27,30 @@ fun CompletedTasksContainer(vm: HistoryScreenViewModel) {
 
     val tasks = vm.getCompletedTasks().observeAsState(listOf())
     val selectedType = vm.selectedTypes.observeAsState(listOf())
+    var emptyTasks by remember { mutableStateOf(false) }
     val all = stringResource(id = R.string.all)
 
     fun parseStringToDate(string: String) = SimpleDateFormat(DATE_FORMAT).parse(string)
 
-        if (tasks.value.isNotEmpty()) {
 
-            var curDate = parseStringToDate(tasks.value[0].deadline)
+    if (tasks.value.isNotEmpty()) {
+        emptyTasks = false
+        var curDate = parseStringToDate(tasks.value[0].deadline)
 
+        if (!emptyTasks) {
             LazyColumn(
                 modifier = Modifier
                     .padding(horizontal = 10.dp)
-                    .fillMaxSize()
             ) {
 
                 itemsIndexed(tasks.value) { index, task ->
 
                     if (selectedType.value.contains(task.taskType.name) || selectedType.value.contains(all)) {
-                        if (curDate != null && curDate != parseStringToDate(task.deadline)) {
+
+                        if (index == 0) {
+                            Text(task.deadline, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 10.dp))
+                            curDate = parseStringToDate(task.deadline)
+                        } else if (curDate != null && curDate != parseStringToDate(task.deadline)) {
                             Text(task.deadline, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 10.dp))
                             curDate = parseStringToDate(task.deadline)
                         }
@@ -52,8 +64,22 @@ fun CompletedTasksContainer(vm: HistoryScreenViewModel) {
                         ) {
                             HistoryScreenTaskContainer(task, vm)
                         }
+
+                    }
+                    else if (index >= tasks.value.size -1) {
+                        Column(
+                            Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center) {
+                            Image(painter = painterResource(id = R.drawable.ic_add_task), modifier = Modifier
+                                .padding(top = 30.dp)
+                                .size(100.dp), contentDescription = "plus sign", colorFilter = ColorFilter.tint(
+                                MaterialTheme.colors.surface))
+                            Text(text = stringResource(R.string.no_tasks_found), Modifier.padding(20.dp), color = MaterialTheme.colors.surface, fontSize = 18.sp, textAlign = TextAlign.Center)
+                        }
                     }
                 }
             }
         }
+    }
 }
