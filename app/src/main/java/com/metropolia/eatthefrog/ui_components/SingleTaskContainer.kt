8b46beaeabca4f952fb7deb8ce2c8e44b71433e1
Sirja Kosonen <sirja.kosonen@gmail.com>
2022-11-24 10:12:36 +0200

    Update SingleTaskContainer location

--- app/src/main/java/com/metropolia/eatthefrog/ui_components/SingleTaskContainer.kt
+++ app/src/main/java/com/metropolia/eatthefrog/ui_components/SingleTaskContainer.kt
@@ -1,7 +1,6 @@
-package com.metropolia.eatthefrog.screens.home.components
+package com.metropolia.eatthefrog.ui_components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -10,27 +9,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.capitalize
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.metropolia.eatthefrog.R
-import com.metropolia.eatthefrog.activities.MainActivity
 import com.metropolia.eatthefrog.database.Task
-import com.metropolia.eatthefrog.placeholder_data.PlaceholderTask
-import com.metropolia.eatthefrog.placeholder_data.PlaceholderTasks
 import com.metropolia.eatthefrog.viewmodels.DateFilter
-import com.metropolia.eatthefrog.viewmodels.HomeScreenViewModel
 import com.metropolia.eatthefrog.viewmodels.TasksViewModel
-import java.util.*
 
 /**
  * Container for a singular task in the Home screen list
