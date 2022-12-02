0c5cc102c0f9b28cb0726962a010ee89404d4dea
Sirja Kosonen <sirja.kosonen@gmail.com>
2022-12-02 13:31:28 +0200

    Translations for notification settings dropdown

--- app/src/main/java/com/metropolia/eatthefrog/viewmodels/NotificationsViewModel.kt
+++ app/src/main/java/com/metropolia/eatthefrog/viewmodels/NotificationsViewModel.kt
@@ -3,7 +3,9 @@ package com.metropolia.eatthefrog.viewmodels
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.MutableLiveData
+import com.metropolia.eatthefrog.R
 import com.metropolia.eatthefrog.constants.DATE_FORMAT
 import com.metropolia.eatthefrog.constants.NOTIFICATION_KEY
 import com.metropolia.eatthefrog.constants.SHARED_PREF_KEY
@@ -16,6 +18,7 @@ import java.time.format.DateTimeFormatter
 
 class NotificationsViewModel(application: Application) : HomeScreenViewModel(application) {
     private val database = InitialDB.get(application)
+    private val stringGetter = getApplication<Application>().resources
     fun getCertainTask(id: Long) = database.taskDao().getTest(id)
 
     private val sharedPreferences: SharedPreferences = application
@@ -26,22 +29,21 @@ class NotificationsViewModel(application: Application) : HomeScreenViewModel(app
     var deadlineValue = MutableLiveData(0)
 
     val listItems = listOf(
-        "At deadline",
-        "5 mins before",
-        "10 mins before",
-        "30 min before",
-        "1 hour before"
+        stringGetter.getString(R.string.notif_at_deadline),
+        stringGetter.getString(R.string.notif_5before),
+        stringGetter.getString(R.string.notif_10before),
+        stringGetter.getString(R.string.notif_30before),
+        stringGetter.getString(R.string.notif_hour_before)
     )
 
     val minutes = mapOf(
-        "At deadline" to 0,
-        "5 mins before" to 5,
-        "10 mins before" to 10,
-        "30 min before" to 30
+        stringGetter.getString(R.string.notif_5before) to 5,
+        stringGetter.getString(R.string.notif_10before) to 10,
+        stringGetter.getString(R.string.notif_30before) to 30
     )
 
     val hours = mapOf(
-        "1 hour before" to 1
+        stringGetter.getString(R.string.notif_hour_before) to 1
     )
 
     init {
